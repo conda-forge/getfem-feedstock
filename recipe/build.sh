0afcdb9b7de8a2cb506ac29ad2f5231969bdfee3
Luca Dall Olio <luca.dallolio@gmail.com>
2026-03-13 22:39:50 +0100

    tentative fixes for both linux and windows

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,16 @@ set -ex
 
 echo "**************** G E T F E M  B U I L D  S T A R T S  H E R E ****************"
 
-#./autogen.sh
-./configure --prefix=$PREFIX --enable-shared --with-pic --enable-python # --enable-octave
+# Downgrade the strict GCC 14 pointer error to a warning
+export CFLAGS="$CFLAGS -Wno-error=incompatible-pointer-types"
+export CXXFLAGS="$CXXFLAGS -Wno-error=incompatible-pointer-types"
+
+./configure \
+    --prefix=$PREFIX \
+    --enable-shared \
+    --with-pic \
+    --enable-python
+
 make -j $CPU_COUNT
 make install
 make check -j $CPU_COUNT
