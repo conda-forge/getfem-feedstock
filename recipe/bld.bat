9e43af24cae29e904b5f7aca6741678451cc2821
Luca Dall Olio <luca.dallolio@gmail.com>
2026-03-14 23:53:39 +0100

    restoring bld.bat in the right format

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,39 +1,37 @@
-#!/usr/bin/env bash
-set -ex
+@echo off
+setlocal EnableDelayedExpansion
 
-echo "**************** G E T F E M  B U I L D  S T A R T S  H E R E ****************"
+echo "Building GetFEM with CMake..."
+FOR /F "delims=" %%i IN ('python -c "import numpy; print(numpy.get_include())"') DO set "NUMPY_INC=%%i"
 
-export CFLAGS="$CFLAGS -Wno-error=incompatible-pointer-types"
-export CXXFLAGS="$CXXFLAGS -Wno-error=incompatible-pointer-types"
+cmake -B build ^
+  -G Ninja ^
+  %CMAKE_ARGS% ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+  -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+  -DENABLE_PYTHON=ON ^
+  -DBUILD_SHARED_LIBS=ON ^
+  -DGENERATE_GETFEM_IM_LIST_H=OFF ^
+  -DENABLE_MULTITHREADED_BLAS=ON ^
+  -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
+  -DMUMPS_INC_DIR="%LIBRARY_INC%" ^
+  -DSMUMPS_LIB="%LIBRARY_LIB%\smumps_seq.lib" ^
+  -DDMUMPS_LIB="%LIBRARY_LIB%\dmumps_seq.lib" ^
+  -DCMUMPS_LIB="%LIBRARY_LIB%\cmumps_seq.lib" ^
+  -DZMUMPS_LIB="%LIBRARY_LIB%\zmumps_seq.lib" ^
+  -DMUMPS_COMMON_LIB="%LIBRARY_LIB%\mumps_common_seq.lib" ^
+  -DPORD_LIB="%LIBRARY_LIB%\pord_seq.lib" ^
+  -DMPISEQ_LIB="%LIBRARY_LIB%\mpiseq_seq.lib" ^
+  -DPython3_EXECUTABLE="%PYTHON%" ^
+  -DPython3_NumPy_INCLUDE_DIRS="%NUMPY_INC%" ^
+  -DBLAS_LIBRARIES="%LIBRARY_LIB%\openblas.lib" ^
+  -DLAPACK_LIBRARIES="%LIBRARY_LIB%\openblas.lib"
 
-if [[ "$target_platform" == osx-* ]]; then
-  EXT=".dylib"
-else
-  EXT=".so"
-fi
-
-NUMPY_INC=$($PYTHON -c "import numpy; print(numpy.get_include())")
-
-cmake -B build \
-    -G Ninja \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-    -DCMAKE_PREFIX_PATH="$PREFIX" \
-    -DENABLE_PYTHON=ON \
-    -DBUILD_SHARED_LIBS=ON \
-    -DGENERATE_GETFEM_IM_LIST_H=OFF \
-    -DMUMPS_INC_DIR="$PREFIX/include" \
-    -DSMUMPS_LIB="$PREFIX/lib/libsmumps_seq${EXT}" \
-    -DDMUMPS_LIB="$PREFIX/lib/libdmumps_seq${EXT}" \
-    -DCMUMPS_LIB="$PREFIX/lib/libcmumps_seq${EXT}" \
-    -DZMUMPS_LIB="$PREFIX/lib/libzmumps_seq${EXT}" \
-    -DMUMPS_COMMON_LIB="$PREFIX/lib/libmumps_common_seq${EXT}" \
-    -DPORD_LIB="$PREFIX/lib/libpord_seq${EXT}" \
-    -DMPISEQ_LIB="$PREFIX/lib/libmpiseq_seq${EXT}" \
-    -DPython3_EXECUTABLE="$PYTHON" \
-    -DPython3_NumPy_INCLUDE_DIRS="$NUMPY_INC"
+if errorlevel 1 exit 1
 
 cmake --build build
-cmake --install build
+if errorlevel 1 exit 1
 
-echo "**************** G E T F E M  B U I L D  E N D S  H E R E ****************"
+cmake --install build
+if errorlevel 1 exit 1
